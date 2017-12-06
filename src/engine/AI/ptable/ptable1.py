aee1efd0f65daa7cff18797a374a960c8414c414
Thiago Vieira de A Silva <thiagovieiraas@gmail.com>
2017-12-05 22:47:43 -0200

    Started implementing two classes to calculate the board's payoff

---
+++ src/engine/AI/ptable/ptable1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#
+
+
+def PTable1():
+  '''
+    This class just serves to calculate the payoff of a given board.
+  '''
+
+  def __init__(self):
+    pass
+  
+  
+  def payoff(self, big_board, small_boards):
+    '''
+      This method calculates the payoff of a given board.
+
+
+       10 |  5  | 10
+      ---------------
+       5  |  20 | 5
+      ---------------
+       10 |  5  | 10
+    '''
+    pass
---
+++ src/engine/AI/ptable/ptable2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#
+
+
+def PTable2():
+  '''
+    This class 
+  '''
+
+  def __init__(self):
+    pass
+  
+  
+  def payoff(self, big_board, small_boards):
+    '''
+      This method calculates the payoff of a given board.
+
+
+       20 |  5  | 20
+      ---------------
+       5  |  10 | 5
+      ---------------
+       20 |  5  | 20
+    '''
+    pass
